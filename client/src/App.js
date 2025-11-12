0f473877061c6da0814a69b49248c84d6f8f5c87
Joy <joykenneth605@gmail.com>
2025-11-12 09:54:11 +0100

    Add CRUD UI and not found page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import { HttpLink } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
-import Clients from "./components/Clients";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -33,10 +36,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-            <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects/:id" element={<Project />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>          
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
